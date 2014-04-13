be5f2f4d5a9f28dfd6277d5fe0c5064c1f2a556f
Trygve Bærland <trygve.baerland@gmail.com>
2014-04-13 16:47:22 +0200

    Laget meshgenerator

--- test.py
+++ test.py
@@ -1,4 +1,14 @@
-from numpy import *
-from quadratures import *
 
-print quadrature1D(1,2,1,exp)
+import meshpy.triangle as triangle
+from numpy import *
+from matplotlib.pyplot import *
+from generate_mesh_first import *
+
+mesh = mesh_for_intersecting_circles(2,0.5)
+pts = vstack(mesh.points)
+elements = vstack(mesh.elements)
+
+triplot(pts[:,0],pts[:,1],elements[:,:3])
+triangle.write_gnuplot_mesh("triangles.dat", mesh)
+show()
+
