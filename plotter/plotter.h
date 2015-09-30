5e03c0fef47814aad27a3933d29123dd2fe24aae
Trygve Bærland <trygve.baerland@gmail.com>
2015-09-30 22:02:57 +0200

    Moved Plotter to separate project.

---
+++ plotter/plotter.h
@@ -0,0 +1,34 @@
+//: plotter.cpp
+#include <iostream>
+#include "plotter.h"
+
+/* Various include statements for plot_line: */
+#include <vtkVersion.h>
+#include <vtkRenderer.h>
+#include <vtkRenderWindowInteractor.h>
+#include <vtkRenderWindow.h>
+#include <vtkSmartPointer.h>
+#include <vtkChartXY.h>
+#include <vtkTable.h>
+#include <vtkPlot.h>
+#include <vtkDoubleArray.h>
+#include <vtkContextView.h>
+#include <vtkContextScene.h>
+#include <vtkPen.h>
+/*-----------------------------------------*/
+
+Plotter::Plotter(){
+	// Do nothing
+}
+
+Plotter::~Plotter(){
+	// Do nothing
+}
+
+/* -------------------------------- */
+/*			PLOT_LINE									  */
+/* -------------------------------- */
+void plot_line(double *x, double *y, int num_points){
+	// Do nothing just yet...
+}
+
