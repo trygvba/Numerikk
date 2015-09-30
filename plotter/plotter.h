c5acd7ff8a0c8f4c2cbf83b23f492086f815a17b
Trygve Bærland <trygve.baerland@gmail.com>
2015-09-30 22:34:21 +0200

    ...

---
+++ plotter/plotter.h
@@ -0,0 +1,12 @@
+//: plotter.h
+#ifndef PLOTTER_H
+#define PLOTTER_H
+
+class Plotter {
+	public:
+		Plotter();
+		~Plotter();
+
+		void plot_line(double* x, double* y, int num_points);
+};
+#endif // PLOTTER_H
