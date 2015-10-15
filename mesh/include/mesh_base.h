cce7256f2e761f3db3df0c213e48eb2f0945c152
Trygvolini <trygve.baerland@gmail.com>
2015-10-15 09:17:37 +0200

    ...

--- mesh/include/mesh_base.h
+++ mesh/include/mesh_base.h
@@ -7,22 +7,22 @@ class MeshBase {
   public:
     // Constructors and destructors
     MeshBase();     // default constructor    
-		MeshBase(struct triangulateio* in);	// constructor taking in triangulateio struct
+    MeshBase(struct triangulateio* in);	// constructor taking in triangulateio struct
     ~MeshBase();
     
-		// Public member functions
-		void print();
+    // Public member functions
+    void print();
   private:
-		// Private member variables:
+    // Private member variables:
     int num_points; // number of points
     int num_tri;    // number of triangles
     double** points; // array with points
     int** elements;   // elements connectivity matrix
 
-		//Private member functions:
-		void create_mesh(struct triangulateio* in);	// To be used by most constructors.
-		void set_points_from_triangleout(struct triangulateio *out); // To set the points
-		void set_elements_from_triangleout(struct triangulateio *out); // set element data.
+    //Private member functions:
+    void create_mesh(struct triangulateio* in);	// To be used by most constructors.
+    void set_points_from_triangleout(struct triangulateio *out); // To set the points
+    void set_elements_from_triangleout(struct triangulateio *out); // set element data.
 };
 
 
