0a65852026b191ef27bde73e48f9fed92ff93925
Trygve Bærland <trygve.baerland@gmail.com>
2016-10-06 14:40:39 +0200

    Implemented some for the FourierFunction class.

---
+++ fourier/include/fourier.hpp
@@ -0,0 +1,26 @@
+// File: fourier.hpp
+#ifndef FOURIER_HPP
+#define FOURIER_HPP
+
+
+/************************************
+ *      FOURIER CLASS:
+ ************************************/
+class FourierFunction {
+    public:
+        FourierFunction();
+        FourierFunction(double* in_eval, int in_N);
+        ~FourierFunction();
+
+        // Getter functions:
+        int get_N();
+        double get_eval(const int i);
+        void get_fourier_coefficient(const int i, double* out, int len);
+    private:
+        int N;
+        double* evals;      // Periodic evaluations
+        double* fcoeffs;    // Fourier coefficients
+        bool owns_evals;    // Ownership flag for evaluations
+}
+
+#endif // FOURIER_HPP
---
+++ fourier/src/fourier.cpp
@@ -0,0 +1,54 @@
+// File: fourier.cpp
+
+#include "fourier.hpp"
+#include <fftw3.h>
+
+/******************************
+ *  FOURIER CLASS FUNCTIONS:
+ ******************************/
+
+FourierFunction::FourierFunction(){
+    N = 0;
+    evals = NULL;
+    fcoeffs = NULL;
+    owns_evals = false;
+}
+
+FourierFunction::FourierFunction(double* in_eval, int in_N){
+    // Number of points is then N:
+    N = in_N;
+    evals = in_eval;
+
+    // Allocate memory for fcoeffs:
+    fcoeffs = new double[2*(N/2+1)];
+    // Set up for FFT:
+    fftw_plan p;
+    p = fftw_plan_dft_r2c_1d(N, evals, fcoeffs);
+    
+    // Execute FFT:
+    fftw_execute(p);
+    // Destroy plan:
+    fftw_destroy_plan(p);
+}
+
+FourierFunction::~FourierFunction(){
+    if (owns_evals && evals){
+        delete [] evals;
+    }
+    if (fcoeffs){
+        delete [] fcoeffs;
+    }
+}
+
+int FourierFunction::get_N(){
+    return N;
+}
+
+double FourierFunction::get_eval(const int i){
+    return evals[i];
+}
+
+void FourierFunction::get_fourier_coefficient(const int i, double* out, int len){
+    out[0] = fcoeffs[2*i];
+    out[1] = fcoeffs[2*i+1];
+}
